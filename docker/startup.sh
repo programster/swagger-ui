fc0eb8ad9636ebdd7f3d5b8d1be5094f9950c859
programster <stuart.page@irap.org>
2016-02-16 16:33:22 +0000

    BUGFIX - the container doesnt always overwrite the swagger spec with the default if one already exists.

--- docker/startup.sh
+++ docker/startup.sh
@@ -2,7 +2,7 @@
 # Script that should execute on startup of the container.
 
 # copy across create the default swagger spec if one doesnt exist.
-if [ ! -f /tmp/foo.txt ]; then
+if [ ! -f /build/dist/spec/swagger-spec.json ]; then
     echo "Couldnt find spec, so creating default."
     cp /build/docker/default-spec.json /build/dist/spec/swagger-spec.json
 fi
